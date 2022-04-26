c955b79e2b1a4862c0eaf9948d8523a97289b39b
kaiserwilli <ka1serw1ll1@gmail.com>
2022-04-26 12:18:00 -0700

    Update MBGInterviewFunctions.js

--- MBGInterviewFunctions.js
+++ MBGInterviewFunctions.js
@@ -2,7 +2,7 @@
 // @name         MBG Interview Functions
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  Add Functionality to MBG
 // @author       You
 // @match        https://siaesolutions.com/ezMandate/mbg.action
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=siaesolutions.com
